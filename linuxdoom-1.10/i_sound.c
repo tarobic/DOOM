665014e8650895889d81b811f26b4367cabdd76d
tarobic <msullentrup1@gmail.com>
2025-11-24 00:43:24 -0600

    Fix compile-preventing errors

--- linuxdoom-1.10/i_sound.c
+++ linuxdoom-1.10/i_sound.c
@@ -36,6 +36,8 @@ static const char rcsid[] = "$Id: i_unix.c,v 1.5 1997/02/03 22:45:10 b1 Exp $";
 #include <sys/filio.h>
 #endif
 
+#include <errno.h>
+
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
--- linuxdoom-1.10/i_video.c
+++ linuxdoom-1.10/i_video.c
@@ -44,7 +44,7 @@ int XShmGetEventBase(Display* dpy); // problems with g++?
 #include <sys/time.h>
 #include <sys/types.h>
 
-#include <errnos.h>
+#include <errno.h>
 #include <netinet/in.h>
 #include <signal.h>
 
@@ -681,7 +681,6 @@ void grabsharedmemory(int size)
 			id = shmget((key_t)key, size, IPC_CREAT | 0777);
 			if (id == -1)
 			{
-				extern int errno;
 				fprintf(stderr, "errno=%d\n", errno);
 				I_Error("Could not get any shared memory");
 			}
